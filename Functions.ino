a756b4342a9f9fb8fa95466215601c655673175d
Josh <31944148+apatakiparadise@users.noreply.github.com>
2017-10-11 11:06:22 +1000

    Added ultrasonic constants

--- Functions.ino
+++ Functions.ino
@@ -14,10 +14,13 @@ void import_statements_etc(){
   #define pin1  9//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
   #define pin2  10
   #define pin3  13
-  #define pin4  2
-  
+  #define pin4  2  
   #define delaytime 10   //delay time in ms to control the stepper motor delaytime.
                         //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
+  
+  //Ultrasonic pins
+  const int trigPin = A2; //Is trigpin what sends out the signal?
+  const int echoPin = A1;//and echopin what receives it?
 
 
 }
