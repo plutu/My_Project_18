ab0a9715f112afa8362bfe0c5377d88007956a83
Josh <31944148+apatakiparadise@users.noreply.github.com>
2017-10-11 11:54:30 +1100

    Changed Functions.ino to include import statements
    Created function import_statements_etc() which has all the code to be run before setup()
    Still needs to be tested, there are a ton of bugs in Functions.ino which I haven't fixed yet.

--- Functions.ino
+++ Functions.ino
@@ -1,7 +1,26 @@
 /* Here we list all functions to be implemented in our code, 
  * with docstrings to explain what it does, what parameters it has and what it returns 
  */
+void import_statements_etc(){
+  //this goes in the top of the main function, before setup()
+  #include <StepperAK.h>
+  #include <AFMotor.h>
 
+  AF_DCMotor motor1(1);
+  AF_DCMotor motor2(2);
+  AF_DCMotor motor3(3);
+  AF_DCMotor motor4(4);
+  
+  #define pin1  9//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
+  #define pin2  10
+  #define pin3  13
+  #define pin4  2
+  
+  #define delaytime 10   //delay time in ms to control the stepper motor delaytime.
+                        //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
+
+
+}
 
 //DC MOTORS
 void dc_move(motor1, motor2, dir, runtime){
---
+++ main.ino
@@ -0,0 +1,14 @@
+
+
+void import_statements_etc();
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
