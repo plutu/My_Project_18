2e802cf9d2fe1f1a100befa8cb8d8375f24287e4
Josh <31944148+apatakiparadise@users.noreply.github.com>
2017-10-13 10:07:03 +1000

    Copied Ellen's changes into sketchbook

--- Arduino/PreTest/main/main.ino
+++ Arduino/PreTest/main/main.ino
@@ -1,11 +1,12 @@
 #include <StepperAK.h>
-  #include <AFMotor.h>
+#include <AFMotor.h>
 
   AF_DCMotor motor1(1);
   AF_DCMotor motor2(2);
   AF_DCMotor motor3(3);
   AF_DCMotor motor4(4);
-  
+
+  //stepper pins
   #define pin1  9//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
   #define pin2  10
   #define pin3  13
@@ -15,13 +16,17 @@
                         //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
 
 
-
-
-
 void setup() {
   // put your setup code here, to run once:
-import_statements_etc();
-
+  
+  //hopefully, these two do the same thing! 
+  dc_move(motor1,motor3,FORWARD,2000); 
+  
+  delay(5000);
+  
+  dc_go(motor1,motor3,FORWARD);
+  delay(2000);
+  dc_stop(motor1,motor3);
 }
 
 void loop() {
