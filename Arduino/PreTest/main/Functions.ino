6c4f8b8bbb2ad684d260a8de2042d8129d4fec4b
Josh <31944148+apatakiparadise@users.noreply.github.com>
2017-10-13 10:05:22 +1000

    Copied Ellen's changes into correct directory

--- Arduino/PreTest/main/Functions.ino
+++ Arduino/PreTest/main/Functions.ino
@@ -1,29 +1,12 @@
 /* Here we list all functions to be implemented in our code, 
  * with docstrings to explain what it does, what parameters it has and what it returns 
  */
-void import_statements_etc(){
-  //this goes in the top of the main function, before setup()
-  #include <StepperAK.h>
-  #include <AFMotor.h>
 
-  AF_DCMotor motor1(1);
-  AF_DCMotor motor2(2);
-  AF_DCMotor motor3(3);
-  AF_DCMotor motor4(4);
-  
-  #define pin1  9//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
-  #define pin2  10
-  #define pin3  13
-  #define pin4  2
-  
-  #define delaytime 10   //delay time in ms to control the stepper motor delaytime.
-                        //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
-
-
-}
+#include <StepperAK.h>
+#include <AFMotor.h>
 
 //DC MOTORS
-void dc_move(motor1, motor2, dir, runtime){
+void dc_move(AF_DCMotor motor1,AF_DCMotor motor2, char dir, int runtime){
   /*Moves two specific motors in the same direction
    * Parameters:
    *    motor1, motor2 : dc motor objects on opposite sides of the vehicle
@@ -34,10 +17,26 @@ void dc_move(motor1, motor2, dir, runtime){
   motor2.setSpeed(255);
   motor1.run(dir);
   motor2.run(dir);
-  delay(runtime);       
+  delay(runtime);       // Yo whats this delay(runtime) doing? why we wanna do this?
   motor1.setSpeed(0);  //Q: Do we want the motor to run continuously until we tell it to stop,
   motor2.setSpeed(0);  //    or do we want it to go a certain distance then stop?
-  delay(500);
+  delay(500); 
+}
+
+/* ELLEN: "Josh, ive written these functions for us to test tomorrow (friday 13th)
+ *  hopefully that'll answer your q abt wheter the run func should specify distance or not
+ */
+ 
+void dc_go(AF_DCMotor A, AF_DCMotor B, char dir){
+  A.setSpeed(255);
+  B.setSpeed(255);
+  A.run(dir);
+  B.run(dir);
+}
+
+void dc_stop(AF_DCMotor A, AF_DCMotor B){
+  A.setSpeed(0);
+  B.setSpeed(0);
 }
 
 void spin(AF_DCMotor motor1, AF_DCMotor motor2, int angle){
@@ -81,6 +80,4 @@ bool is_mag (){
    *  Depending on how the magnetometer works for us, we may change what this function does.
    *  Could end up being some sort of get_mag_distance()  Annie?   */
 }
-
-
 
