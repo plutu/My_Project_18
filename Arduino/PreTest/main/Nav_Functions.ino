787b0200e58cd43c32405baa0d420ec028d39c59
Josh <31944148+apatakiparadise@users.noreply.github.com>
2017-10-18 23:13:07 +1100

    Line by line algorithm
    Added nav_functions, to break down the code, added some needful constants to top of main.
    
    main loop: go along each line, if there's something magnetic in front, pick it up, otherwise go left along the line, then right, then back to base to drop the goods off. then go to the next line.
    
    To do: add a conditional statement so that if no stuff is collected, we advance instead of needlessly dumping.

---
+++ Arduino/PreTest/main/Nav_Functions.ino
@@ -0,0 +1,33 @@
+//This is where we put our overall navigational functions
+
+void strip(char dir){
+  dc_go(motor2, motor4, dir);
+  howfarwevecome = 0;
+  while(howfarwevecome<sidewaystime){
+   if (get_distance()<5){
+    dc_stop(motor2, motor4);
+    if(is_mag()=True){
+      collect();  
+    }
+    delay(pausetime);
+  }
+  howfarwevecome += pausetime;  
+  }
+  dc_stop(motor2, motor4);
+}
+
+void backtobase(int lines){//do we even need this function?
+  dc_move(motor1, motor3, BACKWARD, lines*linesdone);
+  //thought: maybe change the dc_move runtime to some arbitrarily large number (10000) so that we just run until we hit the wall. 
+  //Can our bot take some bumps?
+}
+
+void dump(){
+  dc_spin(motor2, motor4, 180); //doesn't matter which two motors, really
+  step_open();
+  dc_move(motor1, motor3, BACKWARD, linewidth);
+  step_close();
+  dc_spin(motor2, motor4, 180);
+}
+
+
--- Arduino/PreTest/main/main.ino
+++ Arduino/PreTest/main/main.ino
@@ -31,5 +31,47 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  dc_move(motor1,motor3,BACKWARD,10000); 
 
 }
+#include <StepperAK.h>
+#include <AFMotor.h>
+
+  AF_DCMotor motor1(1);//forward/backward
+  AF_DCMotor motor2(2);//sideways
+  AF_DCMotor motor3(3);//forward/backward
+  AF_DCMotor motor4(4);//sideways
+
+  //stepper pins
+  #define pin1  9//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
+  #define pin2  10
+  #define pin3  13
+  #define pin4  2
+  
+  #define delaytime 10   //delay time in ms to control the stepper motor delaytime.
+                        //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
+
+  int sidewaystime = 3000; //how long does it take to reach the other side? (test this)
+  int howfarwevecome = 0;
+  int pausetime = 100; //this is how long we wait in between ultrasonic measurements
+  int linewidth = 300; //in ms, this is how far we advance per line
+  int linesdone = 0
+  int numberofstepstoopendoor = 90 //need to test this experimentally
+void setup() {
+  // put your setup code here, to run once:
+  
+ 
+}
+
+void loop() {
+  linesdone+=1
+  strip(FORWARD);
+  strip(BACKWARD); //make sure that we end up on the right hand edge, so that we don't get stuck around the corner.
+  backtobase(linesdone);
+  dump(); //dump should include spinning, opening door, reversing, closing door, then spinning again
+  /*GENIUS: we can make the reverse part of dump exactly linewidth, which will mean that when we go back to the frontlines
+  we'll be one line further forward */
+  dc_move(motor1, motor3, FORWARD, linesdone*linewidth)
+  
+}
+
