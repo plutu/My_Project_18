f7421362d071d15cb3ba6335e73a79f335a41cc2
annie202 <32503227+annie202@users.noreply.github.com>
2017-10-19 16:19:14 +1000

    Update Functions.ino

--- Arduino/PreTest/main/Functions.ino
+++ Arduino/PreTest/main/Functions.ino
@@ -133,10 +133,64 @@ int get_distance(){
 
 
 
-bool is_mag (){
-  /* Returns true if nearest object is magnetic, false otherwise.
-   *  Depending on how the magnetometer works for us, we may change what this function does.
-   *  Could end up being some sort of get_mag_distance()  Annie?   */
+ void is_mag (){
+  /* Calculates the disturbance in the magnetic field of the Earth due to the presence of a magnet 
+  and returns a bool value .   */
+ #include <Wire.h> //I2C Arduino Library
+
+#define address 0x1E //0011110b, I2C 7bit address of HMC5883
+
+void setup(){
+  //Initialize Serial and I2C communications
+  Serial.begin(9600);
+  Wire.begin();
+  
+  //Put the HMC5883 IC into the correct operating mode
+  Wire.beginTransmission(address); //open communication with HMC5883
+  Wire.write(0x02); //select mode register
+  Wire.write(0x00); //continuous measurement mode
+  Wire.endTransmission();
+}
+
+
+void loop(){
+  
+  int x,y,z; //triple axis data
+
+  //Tell the HMC5883L where to begin reading data
+  Wire.beginTransmission(address);
+  Wire.write(0x03); //select register 3, X MSB register
+  Wire.endTransmission();
+  
+ 
+ //Read data from each axis, 2 registers per axis
+  Wire.requestFrom(address, 6);
+  if(6<=Wire.available()){
+    x = Wire.read()<<8; //X msb
+    x |= Wire.read(); //X lsb
+    z = Wire.read()<<8; //Z msb
+    z |= Wire.read(); //Z lsb
+    y = Wire.read()<<8; //Y msb
+    y |= Wire.read(); //Y lsb
+  }
+  
+  
+  //Print out values of each axis
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print("  y: ");
+  Serial.print(y);
+  Serial.print("  z: ");
+  Serial.println(z);
+  
+  if (x>200 && y>200 && z>200 || x<-200 && y<-200 && z<-200 ){
+    Serial.print("Target is  magnetic ");
+  }
+   
+  
+  delay(250);
+}
+	
 }
 
 bool are_we_home(){
